3f38fed4ad453eb081de950ea4fb14220fbf2674
Ray <rayanebarbosa.dev@gmail.com>
2025-09-09 20:37:58 +0000

    feat: create CarrinhoCompra class

--- app_carrinho_compras/index.php
+++ app_carrinho_compras/index.php
@@ -4,4 +4,33 @@
 
     use App\CarrinhoCompra;
 
-    echo CarrinhoCompra::teste();
+    $carrinho1 = new CarrinhoCompra();
+    print_r($carrinho1->showItems());
+
+    echo 'Valor total: ' .$carrinho1-> totalPrice();
+
+   $carrinho1->addItems('Bicicleta', '760.10');
+   $carrinho1->addItems('XBOX','900.00');
+   $carrinho1->addItems('Tapete','50.00');
+
+   echo "<br/>";
+   print_r($carrinho1->showItems());
+   echo 'Valor total recalculado: '.$carrinho1->totalPrice();
+
+   echo "<br/>";
+   echo 'Status: '.$carrinho1->showStatus();
+
+   echo '<br/>';
+   $carrinho1->finishOrder();
+   echo 'Status: '.$carrinho1->showStatus();
+
+   if($carrinho1->validateCart()){
+     echo 'Pedido realizado com sucesso!';
+   }
+   else{
+    echo '<br/> Erro na confirmação do pedido. Carrinho não possui itens.';
+   }
+
+   echo'<br/>';
+   echo 'status: ' .$carrinho1->showStatus();
+
--- app_carrinho_compras/src/CarrinhoCompra.php
+++ app_carrinho_compras/src/CarrinhoCompra.php
@@ -2,7 +2,52 @@
     namespace App;
 
     class CarrinhoCompra {
-        public static function teste() {
-            return 'Hello World!';
+
+        private $items;
+        private $status;
+        private $totalPrice;
+
+
+        public function __construct() {
+            $this->items = [];
+            $this->status = 'aberto';
+            $this->totalPrice = 0;
+
+
+        }
+        public function showItems(){
+            return $this->items;
+        }
+
+        public function addItems(string $item, float $valor ){
+            array_push($this->items, ["item" => $item,"valor"=> $valor ]);
+            $this->totalPrice += $valor;
+            return $this;
+        }
+      
+        public function totalPrice(){
+            return $this->totalPrice;
+        }
+
+        public function showStatus(){
+            return $this->status;
+        }
+
+        public function finishOrder(){
+
+            if($this->validateCart()){
+            $this->status = "Pedido Confirmado!";
+            $this->sendConfirmationEmail();
+            return true;
+            }
+        }
+
+
+        public function sendConfirmationEmail(){
+            echo '... envia e-mail de confirmação... <br/>';
+        }
+
+        public function validateCart(){
+            return count($this->items) > 0;
+        }
     }
-}
